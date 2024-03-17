3f53a77f685bb30ecfc71cd590997036cf262871
Vennisa Owusu-Barfi <vennisabarfi@gmail.com>
2024-03-17 14:20:31 -0400

    Create happy_new_year_counter.java

---
+++ happy_new_year_counter.java
@@ -0,0 +1,12 @@
+// New Year Countdown timer
+public class happy_new{
+    public static void main(String[] args) {
+        for(int i = 10; i>=0; i--){
+            System.out.println(i);
+        }
+        System.out.println("Happy New Year!");
+       
+        }
+    }
+
+
